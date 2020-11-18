786d333675cf31137027b8de16667500d9b2d317
Thomas Gardella <thomasgardella@gmail.com>
2020-11-17 23:56:59 -0600

    changes to Lily58 and bdn9

---
+++ keyboards/keebio/bdn9/keymaps/gardella/keymap.c
@@ -0,0 +1,193 @@
+/* Copyright 2019 Danny Nguyen <danny@keeb.io>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+enum custom_keycodes {
+  MOOM0 = SAFE_RANGE,
+  MOOM1,
+  MOOM2,
+  MOOM3,
+  MOOM4,
+  MOOM5,
+  MOOM6,
+  MOOM7,
+  MOOM8,
+  MOOM9,
+  ONEP,
+  TWO,
+  TWOTAP,
+  MICMUTE
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case MOOM0:
+      if (record->event.pressed) {
+        // when keycode QMKBEST is pressed
+        SEND_STRING(SS_DOWN(X_F2)SS_DELAY(500)SS_TAP(X_0)SS_UP(X_F2));
+      } else {
+        // when keycode QMKBEST is released
+      }
+      break;
+    case MOOM1:
+      if (record->event.pressed) {
+        // when keycode QMKBEST is pressed
+        SEND_STRING(SS_DOWN(X_F2)SS_DELAY(500)SS_TAP(X_1)SS_UP(X_F2));
+      } else {
+        // when keycode QMKBEST is released
+      }
+      break;
+    case MOOM2:
+      if (record->event.pressed) {
+        // when keycode QMKBEST is pressed
+        SEND_STRING(SS_DOWN(X_F2)SS_DELAY(500)SS_TAP(X_2)SS_UP(X_F2));
+      } else {
+        // when keycode QMKBEST is released
+      }
+      break;
+    case MOOM3:
+      if (record->event.pressed) {
+        // when keycode QMKBEST is pressed
+        SEND_STRING(SS_DOWN(X_F2)SS_DELAY(500)SS_TAP(X_3)SS_UP(X_F2));
+      } else {
+        // when keycode QMKBEST is released
+      }
+      break;
+    case MOOM4:
+      if (record->event.pressed) {
+        // when keycode QMKBEST is pressed
+        SEND_STRING(SS_DOWN(X_F2)SS_DELAY(500)SS_TAP(X_4)SS_UP(X_F2));
+      } else {
+        // when keycode QMKBEST is released
+      }
+      break;
+    case MOOM5:
+      if (record->event.pressed) {
+        // when keycode QMKBEST is pressed
+        SEND_STRING(SS_DOWN(X_F2)SS_DELAY(500)SS_TAP(X_5)SS_UP(X_F2));
+      } else {
+        // when keycode QMKBEST is released
+      }
+      break;
+    case MOOM6:
+      if (record->event.pressed) {
+        // when keycode QMKBEST is pressed
+        SEND_STRING(SS_DOWN(X_F2)SS_DELAY(500)SS_TAP(X_6)SS_UP(X_F2));
+      } else {
+        // when keycode QMKBEST is released
+      }
+      break;
+    case MOOM7:
+      if (record->event.pressed) {
+        // when keycode QMKBEST is pressed
+        SEND_STRING(SS_DOWN(X_F2)SS_DELAY(500)SS_TAP(X_7)SS_UP(X_F2));
+      } else {
+        // when keycode QMKBEST is released
+      }
+      break;
+    case MOOM8:
+      if (record->event.pressed) {
+        // when keycode QMKBEST is pressed
+        SEND_STRING(SS_DOWN(X_F2)SS_DELAY(500)SS_TAP(X_8)SS_UP(X_F2));
+      } else {
+        // when keycode QMKBEST is released
+      }
+      break;
+    case MOOM9:
+      if (record->event.pressed) {
+        // when keycode QMKBEST is pressed
+        //SEND_STRING(SS_DOWN(X_F2)SS_TAP(X_9)SS_UP(X_F2));
+        SEND_STRING(SS_DOWN(X_F2)SS_DELAY(500)SS_TAP(X_9)SS_UP(X_F2));
+      } else {
+        // when keycode QMKBEST is released
+      }
+      break;
+    case ONEP:
+      if (record->event.pressed) {
+        // when keycode QMKBEST is pressed
+        SEND_STRING("D!@b3t3s5250");
+      } else {
+        // when keycode QMKBEST is released
+      }
+      break;
+    case TWO:
+      if (record->event.pressed) {
+        // when keycode QMKBEST is pressed
+        SEND_STRING("Ph@nt0mT!20");
+      } else {
+        // when keycode QMKBEST is released
+      }
+      break;
+    case TWOTAP:
+      if (record->event.pressed) {
+        // when keycode QMKBEST is pressed
+	SEND_STRING(SS_DOWN(X_LSHIFT)SS_TAP(X_B)SS_TAP(X_1)SS_UP(X_LSHIFT)SS_TAP(X_K)SS_TAP(X_I)SS_TAP(X_N)SS_TAP(X_G)SS_DOWN(X_LSHIFT)SS_TAP(X_R)SS_UP(X_LSHIFT)SS_TAP(X_1)SS_TAP(X_V)SS_TAP(X_E)SS_TAP(X_R));
+      } else {
+        // when keycode QMKBEST is released
+      }
+      break;
+    case MICMUTE:
+      if (record->event.pressed) {
+        // when keycode QMKBEST is pressed
+        SEND_STRING(SS_DOWN(X_LCMD)SS_DOWN(X_LCTRL)SS_TAP(X_BSLS)SS_UP(X_LCMD)SS_UP(X_LCTRL));
+      } else {
+        // when keycode QMKBEST is released
+      }
+      break;
+  }
+  return true;
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+        | Knob 1: Vol Dn/Up |               | Knob 2: Brightness Dn/Up |
+        | Press: Mute       | Hold: Layer 2 | Press: Play/Pause        |
+        | MOOM1             | MOOM3         | MOOM5                    |
+        | MOOM2             | MOOM4         | MOOM6                    |
+     */
+    [0] = LAYOUT(
+	KC__MUTE, MO(1), KC_MPLY,
+        MOOM1, MOOM3, MOOM5,
+        MOOM2, MOOM4, MOOM6
+    ),
+    /*
+        | RESET    | Held: Layer 2   | Media Stop |
+        | MOOM7    | MOOM8           | MOOM9  |
+        | TWOTAP   | MOOM0           | ONEP |
+     */
+    [1] = LAYOUT(
+        RESET, _______, KC_STOP,
+        MOOM7, MOOM8, MOOM9,
+        TWO, MOOM0, ONEP
+    ),
+};
+
+void encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) {
+        if (clockwise) {
+            tap_code(KC__VOLDOWN);
+        } else {
+            tap_code(KC__VOLUP);
+        }
+    }
+    else if (index == 1) {
+        if (clockwise) {
+            tap_code(KC_BRMD);
+        } else {
+            tap_code(KC_BRMU);
+        }
+    }
+}
---
+++ keyboards/lily58/keymaps/grdela_1/keymap.c
@@ -0,0 +1,383 @@
+#include QMK_KEYBOARD_H
+
+#ifdef PROTOCOL_LUFA
+  #include "lufa.h"
+  #include "split_util.h"
+#endif
+#ifdef SSD1306OLED
+  #include "ssd1306.h"
+#endif
+
+
+
+#ifdef RGBLIGHT_ENABLE
+//Following line allows macro to read current RGB settings
+extern rgblight_config_t rgblight_config;
+#endif
+
+extern uint8_t is_master;
+
+#define _QWERTY 0
+#define _LOWER 1
+#define _RAISE 2
+#define _ADJUST 3
+
+enum custom_keycodes {
+  QWERTY = SAFE_RANGE,
+  LOWER,
+  RAISE,
+  ADJUST,
+  ALTSFT,
+  MICMT,
+  SCSHT,
+  SCSHTS,
+  NEWTAB,
+  TABRGT,
+  TABLFT,
+  KILTAB,
+  APPSWP,
+  RSPC,
+  LSPC,
+  CUT,
+  COPY,
+  PST,
+  PSTNFM,
+  NOTIF
+  
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+/* QWERTY
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  =   |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  -   |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |LCTRL |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
+ * |------+------+------+------+------+------|   [   |    |   ]   |------+------+------+------+------+------|
+ * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |ENTER |
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *                   | LAlt |  F2  |LGUI  | /Lower  /       \SPACE \  |BackSP|RAISE |AltEnt|
+ *                   |      |      |      |/       /         \      \ |      |      |      |
+ *                   `----------------------------'           '------''--------------------'
+ */
+
+ [_QWERTY] = LAYOUT( \
+  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQUAL, \
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
+  KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_ENT, \
+  KC_LALT, KC_F2, KC_LGUI, LOWER,    KC_SPC,   KC_BSPC, RAISE  , LALT(KC_ENT) \
+),
+
+/* LOWER
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * | ESC  |      |      |      |      |PrtScr|                    |      |      |      |      |      |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |SPACE | LSPC |KILTAB| RSPC |SSSAVE|                    | PGUP |      |  UP  |  [{  |  ]}  |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |BACKSP|TABLFT|NEWTAB|TABRGT|  SS  |-------.    ,-------| PGDN | LEFT | DOWN |RIGHT |  |   |      |
+ * |------+------+------+------+------+------|   [   |    |   ]   |------+------+------+------+------+------|
+ * |      |ENTER | CUT  | COPY | PST  |PSTNFM|-------|    |-------|NOTIF |      |   ,<  | .>  |  \   |ENTER |
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *                   | LAlt |ALTSFT|LGUI  | /Lower  /       \SPACE \  |BackSP|RAISE |SSSAVE|
+ *                   |      |      |      |/       /         \      \ |      |      |      |
+ *                   `----------------------------'           '------''--------------------'
+ */
+[_LOWER] = LAYOUT( \
+  KC_ESC, _______, _______, _______, _______, KC_PSCR,                     _______, _______, _______, _______, _______,  _______, \
+  _______, KC_SPACE, LSPC, LGUI(KC_W), RSPC, SCSHTS,                   KC_PGUP, _______, KC_UP, KC_LBRC, KC_RBRC, _______, \
+  _______, KC_BSPC, LGUI(LALT(KC_LEFT)), LGUI(KC_T), LGUI(LALT(KC_RIGHT)), SCSHT,                   KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_PIPE, _______, \
+  _______, KC_ENT, LGUI(KC_X), LGUI(KC_C), LGUI(KC_V), PSTNFM, _______, _______, NOTIF, _______, _______, _______, KC_BSLS, _______, \
+                             _______, ALTSFT, _______, _______, _______,  _______, _______, SCSHTS \
+),
+/* RAISE
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * |   `  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |  F1  |  F2  |  F3  | F4 |  F5  |  F6  |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |      |      |      |      |      |-------.    ,-------|      |      |      |      |      |      |
+ * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
+ * |      |      |      |      |      |      |-------|    |-------|      |      |      |      |      |      |
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *                   | LAlt |ALTSFT|LGUI  | /Lower  /       \SPACE \  |BackSP|RAISE |SSSAVE|
+ *                   |      |      |      |/       /         \      \ |      |      |      |
+ *                   `----------------------------'           '------''--------------------'
+ */
+
+[_RAISE] = LAYOUT( \
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX,   _______, _______,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, \
+                             _______, _______, _______,  _______, _______,  _______, _______, _______ \
+),
+/* ADJUST
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |      |      |      |      |      |-------.    ,-------|      |      |RGB ON| HUE+ | SAT+ | VAL+ |
+ * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
+ * |      |      |      |      |      |      |-------|    |-------|      |      | MODE | HUE- | SAT- | VAL- |
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
+ *                   |      |      |      |/       /         \      \ |      |      |      |
+ *                   `----------------------------'           '------''--------------------'
+ */
+  [_ADJUST] = LAYOUT( \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD,\
+                             _______, _______, _______, _______, _______,  _______, _______, _______ \
+  )
+};
+
+int RGB_current_mode;
+
+// Setting ADJUST layer RGB back to default
+void update_tri_layer_RGB(uint8_t layer1, uint8_t layer2, uint8_t layer3) {
+  if (IS_LAYER_ON(layer1) && IS_LAYER_ON(layer2)) {
+    layer_on(layer3);
+  } else {
+    layer_off(layer3);
+  }
+}
+
+void matrix_init_user(void) {
+    #ifdef RGBLIGHT_ENABLE
+      RGB_current_mode = rgblight_config.mode;
+    #endif
+    //SSD1306 OLED init, make sure to add #define SSD1306OLED in config.h
+    #ifdef SSD1306OLED
+        iota_gfx_init(!has_usb());   // turns on the display
+    #endif
+}
+
+//SSD1306 OLED update loop, make sure to add #define SSD1306OLED in config.h
+#ifdef SSD1306OLED
+
+// When add source files to SRC in rules.mk, you can use functions.
+const char *read_layer_state(void);
+const char *read_logo(void);
+void set_keylog(uint16_t keycode, keyrecord_t *record);
+const char *read_keylog(void);
+const char *read_keylogs(void);
+
+// const char *read_mode_icon(bool swap);
+// const char *read_host_led_state(void);
+// void set_timelog(void);
+// const char *read_timelog(void);
+
+void matrix_scan_user(void) {
+   iota_gfx_task();
+}
+
+void matrix_render_user(struct CharacterMatrix *matrix) {
+  if (is_master) {
+    // If you want to change the display of OLED, you need to change here
+    matrix_write_ln(matrix, read_layer_state());
+    matrix_write_ln(matrix, read_keylog());
+    matrix_write_ln(matrix, read_keylogs());
+    //matrix_write_ln(matrix, read_mode_icon(keymap_config.swap_lalt_lgui));
+    //matrix_write_ln(matrix, read_host_led_state());
+    //matrix_write_ln(matrix, read_timelog());
+  } else {
+    matrix_write(matrix, read_logo());
+  }
+}
+
+void matrix_update(struct CharacterMatrix *dest, const struct CharacterMatrix *source) {
+  if (memcmp(dest->display, source->display, sizeof(dest->display))) {
+    memcpy(dest->display, source->display, sizeof(dest->display));
+    dest->dirty = true;
+  }
+}
+
+void iota_gfx_task_user(void) {
+  struct CharacterMatrix matrix;
+  matrix_clear(&matrix);
+  matrix_render_user(&matrix);
+  matrix_update(&display, &matrix);
+}
+#endif//SSD1306OLED
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (record->event.pressed) {
+#ifdef SSD1306OLED
+    set_keylog(keycode, record);
+#endif
+    // set_timelog();
+  }
+
+  switch (keycode) {
+    case QWERTY:
+      if (record->event.pressed) {
+        set_single_persistent_default_layer(_QWERTY);
+      }
+      return false;
+      break;
+    case LOWER:
+      if (record->event.pressed) {
+        layer_on(_LOWER);
+        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_LOWER);
+        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case RAISE:
+      if (record->event.pressed) {
+        layer_on(_RAISE);
+        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_RAISE);
+        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case ADJUST:
+        if (record->event.pressed) {
+          layer_on(_ADJUST);
+        } else {
+          layer_off(_ADJUST);
+        }
+        return false;
+        break;
+    case ALTSFT:
+      if (record->event.pressed) {
+        // when keycode ALTSFT is pressed
+        SEND_STRING(SS_DOWN(X_LALT)SS_DOWN(X_LSHIFT));
+      } else {
+        // when keycode ALTSFT is released
+        SEND_STRING(SS_UP(X_LALT)SS_UP(X_LSHIFT));
+      }
+      break;
+    case MICMT:
+      if (record->event.pressed) {
+        // when keycode QMKBEST is pressed
+        SEND_STRING(SS_DOWN(X_LCMD)SS_DOWN(X_LCTRL)SS_TAP(X_BSLS)SS_UP(X_LCMD)SS_UP(X_LCTRL));
+      } else {
+        // when keycode QMKBEST is released
+      }
+      break;
+    case SCSHT:
+      if (record->event.pressed) {
+        // when keycode QMKBEST is pressed
+        SEND_STRING(SS_DOWN(X_LCMD)SS_DOWN(X_LCTRL)SS_DOWN(X_LSFT)SS_TAP(X_4)SS_UP(X_LCMD)SS_UP(X_LCTRL)SS_UP(X_LSFT));
+      } else {
+        // when keycode QMKBEST is released
+      }
+      break;
+    case SCSHTS:
+      if (record->event.pressed) {
+        // when keycode QMKBEST is pressed
+        SEND_STRING(SS_DOWN(X_LCMD)SS_DOWN(X_LSFT)SS_TAP(X_4)SS_UP(X_LCMD)SS_UP(X_LSFT));
+      } else {
+        // when keycode QMKBEST is released
+      }
+      break;
+    case NEWTAB:
+      if (record->event.pressed) {
+        // when keycode QMKBEST is pressed
+        //SEND_STRING(SS_DOWN(X_LCMD)SS_TAP(X_T)SS_UP(X_LCMD));
+      } else {
+        // when keycode QMKBEST is released
+      }
+      break;
+    case KILTAB:
+      if (record->event.pressed) {
+        // when keycode QMKBEST is pressed
+        SEND_STRING(SS_DOWN(X_LCMD)SS_TAP(X_W)SS_UP(X_LCMD));
+      } else {
+        // when keycode QMKBEST is released
+      }
+      break;
+    case TABRGT:
+      if (record->event.pressed) {
+        // when keycode QMKBEST is pressed
+        SEND_STRING(SS_DOWN(X_LCMD)SS_DOWN(X_LALT)SS_TAP(X_RIGHT)SS_UP(X_LCMD)SS_UP(X_LALT));
+      } else {
+        // when keycode QMKBEST is released
+      }
+      break;
+    case TABLFT:
+      if (record->event.pressed) {
+        // when keycode QMKBEST is pressed
+        SEND_STRING(SS_DOWN(X_LCMD)SS_DOWN(X_LALT)SS_TAP(X_LEFT)SS_UP(X_LCMD)SS_UP(X_LALT));
+      } else {
+        // when keycode QMKBEST is released
+      }
+      break;
+    case APPSWP:
+      if (record->event.pressed) {
+        // when keycode QMKBEST is pressed
+        SEND_STRING(SS_DOWN(X_LCMD)SS_TAP(X_TAB)SS_UP(X_LCMD));
+      } else {
+        // when keycode QMKBEST is released
+      }
+      break;
+    case LSPC:
+      if (record->event.pressed) {
+        // when keycode QMKBEST is pressed
+        SEND_STRING(SS_DOWN(X_LCTRL)SS_TAP(X_LEFT)SS_UP(X_LCTRL));
+      } else {
+        // when keycode QMKBEST is released
+      }
+      break;
+    case RSPC:
+      if (record->event.pressed) {
+        // when keycode QMKBEST is pressed
+        SEND_STRING(SS_DOWN(X_LCTRL)SS_TAP(X_RIGHT)SS_UP(X_LCTRL));
+      } else {
+        // when keycode QMKBEST is released
+      }
+      break;
+    case CUT:
+      if (record->event.pressed) {
+        // when keycode QMKBEST is pressed
+        SEND_STRING(SS_DOWN(X_LCMD)SS_TAP(X_X)SS_UP(X_LCMD));
+      } else {
+        // when keycode QMKBEST is released
+      }
+      break;
+    case COPY:
+      if (record->event.pressed) {
+        // when keycode QMKBEST is pressed
+        SEND_STRING(SS_DOWN(X_LCMD)SS_TAP(X_C)SS_UP(X_LCMD));
+      } else {
+        // when keycode QMKBEST is released
+      }
+      break;
+    case PST:
+      if (record->event.pressed) {
+        // when keycode QMKBEST is pressed
+        SEND_STRING(SS_DOWN(X_LCMD)SS_TAP(X_V)SS_UP(X_LCMD));
+      } else {
+        // when keycode QMKBEST is released
+      }
+      break;
+    case PSTNFM:
+      if (record->event.pressed) {
+        // when keycode QMKBEST is pressed
+        SEND_STRING(SS_DOWN(X_LCMD)SS_DOWN(X_LSFT)SS_TAP(X_V)SS_UP(X_LSFT)SS_UP(X_LCMD));
+      } else {
+        // when keycode QMKBEST is released
+      }
+      break;
+    case NOTIF:  //open notifications on mac
+      if (record->event.pressed) {
+        // when keycode QMKBEST is pressed
+        SEND_STRING(SS_DOWN(X_LCTRL)SS_DOWN(X_LSFT)SS_TAP(X_GRAVE)SS_UP(X_LSFT)SS_UP(X_LCTRL));
+      } else {
+        // when keycode QMKBEST is released
+      }
+      break;
+  }
+  return true;
+}
